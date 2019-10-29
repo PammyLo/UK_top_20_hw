0b9024620291766235debe4f2773166035d4ea61
PammyLo <pamela@tuminds.com>
2019-10-29 16:07:05 +0000

    structure added and SongContainer rendering

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import './App.css';
+import SongContainer from './containers/SongContainer';
 
 function App() {
   return (
-    <h1>Hello world</h1>
+    <SongContainer />
   );
 }
 
---
+++ src/containers/SongContainer.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class SongContainer extends Component {
+  render() {
+    return(
+      <h1> this is SongContainer </h1>
+    );
+  }
+}
+
+export default SongContainer;
