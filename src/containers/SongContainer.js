f9034f7a8fdfdf77b70fb6dc9bc7c5627fda260f
PammyLo <pamela@tuminds.com>
2019-10-29 16:21:59 +0000

    api working

--- src/containers/SongContainer.js
+++ src/containers/SongContainer.js
@@ -2,9 +2,27 @@ import React, { Component } from 'react';
 import SongList from '../components/SongList'
 
 class SongContainer extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      top_20_chart: [],
+      currentSong: null
+    }
+  }
+
+  componentDidMount() {
+    const url = "https://itunes.apple.com/gb/rss/topsongs/limit=20/json";
+
+    fetch(url)
+      .then(res => res.json())
+      .then(chart => this.setState({ top_20_chart: chart }))
+      .catch(err => console.error(err));
+  }
+
+
   render() {
     return(
-      <SongList /> 
+      <SongList />
     );
   }
 }
